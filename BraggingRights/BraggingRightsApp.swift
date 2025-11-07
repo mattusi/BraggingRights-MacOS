8395c39e90d39a1846594df51c096b44e13736cf
Matheus Tusi <matheus.tusi@fox.com>
2025-11-07 10:44:22 -0300

    Initial Commit

---
+++ BraggingRights/BraggingRightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BraggingRightsApp.swift
+//  BraggingRights
+//
+//  Created by Matheus Tusi on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BraggingRightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
